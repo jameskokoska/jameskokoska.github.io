1697dd612cde60dadc9dd2ae219c6fb3b3d6db0c
jameskokoska <jameskokoska@users.noreply.github.com>
2020-08-22 03:56:31 -0400

    added boostrap reactive navbar

---
+++ data.js
@@ -0,0 +1,27 @@
+var experience = [
+    {
+        "software":"",
+        "softwareLink":"",
+        "linkGithub":"true",
+    },
+]
+
+var timeline = [
+    {
+        "title":"Academic Mentor, UofT Engineering Academy",
+        "date":"Summer 2020",
+        "description":"\
+        <li><p>Responsible for organizing, creating, and distributing lesson plans based on a curriculum</p></li>\
+        <li><p>Prepared the incoming class of engineers academically in mathematical topics</p></li>\
+        ",
+    },
+    {
+        "title":"Web Team SubCom, UofT Engineering Frosh Orientation Week",
+        "date":"Summer 2020",
+        "description":"\
+        <li><p>Intuitively designed, developed, and programmed various webpages promoting Frosh </p></li>\
+        <li><p>Programmed front-end and back-end functionality as part of a team of engineers</p></li>\
+        <li><p>Incorporated guidelines and considered accessibility for upcoming orientation events</p></li>\
+        ",
+    }
+]
